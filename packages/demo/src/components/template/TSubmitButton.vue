3f36880bdead4aee59c1ee3a1c0546724b7e11aa
Edie Lemoine <edie@myparcel.nl>
2022-10-21 11:51:03 +0200

    fix(demo): fix form submit

--- packages/demo/src/components/template/TSubmitButton.vue
+++ packages/demo/src/components/template/TSubmitButton.vue
@@ -2,19 +2,22 @@
   <a
     role="button"
     class="bg-pink-600 hover:bg-pink-700 inline-flex mt-2 px-5 py-3 rounded-full text-gray-900"
-    @click="form.submit">
+    @click="form?.submit">
     Submit
   </a>
 </template>
 
 <script lang="ts">
 import {defineComponent, inject} from 'vue';
+import {INJECT_FORM} from '@myparcel/vue-form-builder';
 
 export default defineComponent({
   name: 'TSubmitButton',
 
-  setup: () => ({
-    form: inject('form'),
-  }),
+  setup: () => {
+    const form = inject(INJECT_FORM);
+
+    return {form};
+  },
 });
 </script>

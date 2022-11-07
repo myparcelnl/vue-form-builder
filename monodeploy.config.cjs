be15a91bbd7f2c8fafe8c52833f79d4bdd7c4cf6
Edie Lemoine <edie@myparcel.nl>
2022-11-07 14:23:29 +0100

    chore: trigger release

--- monodeploy.config.cjs
+++ monodeploy.config.cjs
@@ -1,4 +1,5 @@
 const branch = require('child_process').execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+
 const prereleaseBranches = ['next', 'alpha', 'beta', 'rc'];
 
 /**
@@ -9,9 +10,10 @@ module.exports = {
   changelogFilename: '<packageDir>/CHANGELOG.md',
   conventionalChangelogConfig: 'conventional-changelog-conventionalcommits',
   persistVersions: true,
-  plugins: [
-    '@monodeploy/plugin-github',
-  ],
+  plugins: ['@monodeploy/plugin-github'],
+  git: {
+    baseBranch: '166be8cadfb8cab31cd4d3153f8f6851261a2454',
+  },
   prerelease: prereleaseBranches.includes(branch),
   prereleaseId: branch,
   prereleaseNPMTag: branch,

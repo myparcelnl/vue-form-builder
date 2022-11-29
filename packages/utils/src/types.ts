c2088f98016996b6d1338895a094dea1c8475688
Edie Lemoine <edie@myparcel.nl>
2022-11-29 17:27:18 +0100

    feat(utils): add FunctionOr type

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -28,3 +28,5 @@ export type IsUnion<T> = [T] extends [UnionToIntersection<T>] ? false : true;
 export type UnionToArray<T, A extends unknown[] = []> = IsUnion<T> extends true
   ? UnionToArray<Exclude<T, PopUnion<T>>, [PopUnion<T>, ...A]>
   : [T, ...A];
+
+export type FunctionOr<T> = (() => T) | T;

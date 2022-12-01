d7d2f13a6465a7f5207e76931fceba7d72da02b9
Edie Lemoine <edie@myparcel.nl>
2022-12-01 09:55:41 +0100

    refactor: clean up form builder definition

--- packages/core/src/composables/useFormBuilder.ts
+++ packages/core/src/composables/useFormBuilder.ts
@@ -4,30 +4,36 @@ import {Ref, reactive, ref} from 'vue';
 let forms: Record<string, FormInstance>;
 let defaults: Ref<Partial<FormConfiguration>>;
 
-type UseFormBuilder = <N extends string, FC extends FormConfiguration = FormConfiguration>() => {
+export type FormBuilder = {
+  /**
+   * All registered forms.
+   */
   forms: typeof forms;
+
+  /**
+   * Default settings to apply to newly registered forms.
+   */
   defaults: Ref<Partial<FormConfiguration>>;
-  register<F extends FC>(name: N, config: F): FormInstance<F>;
+
+  /**
+   * Register a new form. If a form with the same name already exists, it will be overwritten.
+   */
+  register<FC extends FormConfiguration = FormConfiguration, N extends string = string>(
+    name: N,
+    config: FC,
+  ): FormInstance<FC>;
 };
+
+export type UseFormBuilder = () => FormBuilder;
 
 export const useFormBuilder: UseFormBuilder = () => {
   forms ??= reactive({});
   defaults ??= ref({});
 
   return {
-    /**
-     * All registered forms.
-     */
     forms,
-
-    /**
-     * Default settings to apply to newly registered forms.
-     */
     defaults,
 
-    /**
-     * Register a new form. If a form with the same name already exists, it will be overwritten.
-     */
     register(name, config) {
       const instance = new Form(name, {
         ...defaults.value,

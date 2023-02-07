a024e65ab4e7c14c628d45a199a24a6d10308115
Edie Lemoine <edie@myparcel.nl>
2023-02-07 15:20:25 +0100

    refactor: remove unused file

--- libs/build/vite/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import {createViteConfig} from './src';
-
-export default createViteConfig({
-  build: {
-    rollupOptions: {
-      external: ['vue', 'vite', 'vitest', 'vite-plugin-dts', '@vitejs/plugin-vue'],
-    },
-  },
-});

364732d580518d77304b393d03ab37f47cf64c68
Edie Lemoine <edie@myparcel.nl>
2023-05-30 10:58:29 +0200

    fix: add FormHook enum to exports

--- apps/vue-form-builder/src/index.ts
+++ apps/vue-form-builder/src/index.ts
@@ -15,6 +15,14 @@ export type {
 
 export type {SelectOption} from '@myparcel-vfb/utils/src';
 
-export {MagicForm, defineField, defineForm, useElement, useForm, useFormBuilder} from '@myparcel-vfb/core/src';
+export {
+  FormHook,
+  MagicForm,
+  defineField,
+  defineForm,
+  useElement,
+  useForm,
+  useFormBuilder,
+} from '@myparcel-vfb/core/src';
 
 export {MyParcelFormBuilderPlugin, createMyParcelFormBuilderPlugin} from '@myparcel-vfb/plugin/src';

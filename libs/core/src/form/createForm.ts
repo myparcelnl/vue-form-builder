9937ea2ecb3dd0f65bc42f9fa1a640d151cb66ca
Edie Lemoine <edie@myparcel.nl>
2023-11-24 12:42:37 +0100

    fix: remove reactive from created form instance

--- libs/core/src/form/createForm.ts
+++ libs/core/src/form/createForm.ts
@@ -1,11 +1,11 @@
-import {type Component, h, reactive, type UnwrapNestedRefs} from 'vue';
+import {type Component, h, markRaw} from 'vue';
 import {useFormBuilder} from '../composables';
 import MagicForm from '../components/MagicForm.vue';
 import {type FormConfiguration, type FormInstance, type InstanceFormConfiguration} from './Form.types';
 
 export interface CreatedForm<FC extends FormConfiguration> {
   Component: Component;
-  instance: UnwrapNestedRefs<FormInstance<InstanceFormConfiguration<FC>>>;
+  instance: FormInstance<InstanceFormConfiguration<FC>>;
 }
 
 export const createForm = <FC extends FormConfiguration>(name: string, config: Omit<FC, 'fields'>): CreatedForm<FC> => {
@@ -13,8 +13,7 @@ export const createForm = <FC extends FormConfiguration>(name: string, config: O
   const form = formBuilder.register(name, config as FC);
 
   return {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    Component: (_, ctx) => h(MagicForm, {form}, ctx.slots),
-    instance: reactive(form),
+    Component: markRaw((_, ctx) => h(MagicForm, {form}, ctx.slots)),
+    instance: form,
   };
 };

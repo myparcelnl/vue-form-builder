f8a48a4d7bc652be5aff302f9b1bbb2a2163091d
Edie Lemoine <edie@myparcel.nl>
2024-01-10 14:38:04 +0100

    fix(demo): change new form view instructions

--- apps/demo/src/views/NewFormView.vue
+++ apps/demo/src/views/NewFormView.vue
@@ -2,9 +2,8 @@
   <div>
     <h1>Form without fields array</h1>
     <p>
-      These forms are defined entirely in the template. The components can be placed anywhere in the template. When
-      <code>wrapper: false</code> is passed when creating the component, you need to manually place the Label and Errors
-      component as well.
+      These forms are defined entirely in the template. The components can be placed anywhere in the template. You can
+      even choose to manually place the Label and Errors components as well.
     </p>
 
     <div class="gap-2 grid grid-cols-2 grid-flow-row mt-4">
@@ -184,7 +183,6 @@ const Email = createField({
   name: 'email',
   label: 'Email',
   component: TTextInput,
-  wrapper: false,
   props: {
     type: 'email',
   },

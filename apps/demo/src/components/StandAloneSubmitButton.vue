d713638c0d922e72e90abf7e020c7de61ba1f68f
Edie Lemoine <edie@myparcel.nl>
2024-01-11 12:30:56 +0100

    fix(demo): stop validation running twice

--- apps/demo/src/components/StandAloneSubmitButton.vue
+++ apps/demo/src/components/StandAloneSubmitButton.vue
@@ -2,8 +2,7 @@
   <TSubmitButton
     v-if="element"
     :element="element"
-    :outline="outline"
-    @click="() => form?.submit()">
+    :outline="outline">
     <template
       v-for="name in Object.keys($slots)"
       :key="name"
@@ -16,11 +15,10 @@
 </template>
 
 <script lang="ts" setup>
-import {useForm, useElement, type PlainElementInstance} from '@myparcel-vfb/core';
+import {useElement, type PlainElementInstance} from '@myparcel-vfb/core';
 import TSubmitButton from './template/TSubmitButton.vue';
 
 defineProps<{outline?: boolean}>();
 
-const form = useForm();
 const element = useElement<PlainElementInstance>();
 </script>
